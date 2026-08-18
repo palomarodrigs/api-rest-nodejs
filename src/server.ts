7abbeb696a0f456ef1ca696a9b58fad020086bba
palomarodrigs <palomarodrigues.0013@gmail.com>
2026-08-17 23:00:06 -0300

    feat(routes): add transactions routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import fastify from 'fastify'
+
 import { env } from './env'
 
+import { transactionsRoutes } from './routes/transactions'
+
 const app = fastify()
+
+app.register(transactionsRoutes, {
+  prefix: '/transactions',
+})
 
 app.get('/hello', async () => {
   return 'Hello World'
