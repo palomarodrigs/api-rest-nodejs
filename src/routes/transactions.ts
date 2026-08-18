d1cb21cdc1789c73e130622f5412ac79f24e4bc9
palomarodrigs <palomarodrigues.0013@gmail.com>
2026-08-17 23:03:45 -0300

    feat(routes): add get endpoint to retrieve transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,14 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
